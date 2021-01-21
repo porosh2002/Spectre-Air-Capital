6d2384242aa1f9dd54e31d95f6ee3f14cdbc494a
porosh2002 <jamilkashem2002@gmail.com>
2021-01-21 21:01:17 +0600

    Cta update 1.0.0

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,12 @@ function Home() {
         <div>
         <div className='Nav_Cta'>
             <Navigation />
+            <p className='Cta_Text'>
+            Spectre Air Capital is the global leader in aircraft leasing
+            </p>
+            <p className='Cta_Btn'>
+            Discover Spectre
+            </p>
         </div>
         </div>
     )
