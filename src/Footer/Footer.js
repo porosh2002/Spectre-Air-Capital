1499f497edc8865199728958be0296d54eb64f51
porosh2002 <jamilkashem2002@gmail.com>
2021-01-25 01:02:53 +0600

    final update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
-        <div>
-            Footer
+        <div className='Footer'>
+            <p className='FooterLink'>© 2021 Spectre Air Capital All rights reserved</p>
+            <Link className='FooterLink' to='#'>Terms and Condition</Link>
+            <Link className='FooterLink' to='#'>Privacy Policy</Link>
         </div>
     )
 }
